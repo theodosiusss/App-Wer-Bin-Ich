eb4a9d95d22cbd81060a22befc0158a0aaa081ee
Theodosiusss <1202@htl.rennweg.at>
2025-12-01 10:31:45 +0100

    SETUp Frontend

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Main from "@/views/Main.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+      {
+          path: '/',
+          name: 'home',
+          component: Main,
+      },
+  ],
 })
 
 export default router
